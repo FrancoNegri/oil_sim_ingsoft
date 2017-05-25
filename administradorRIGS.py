549a8f6b5c73075d24f17380af141e0bedffa0d6
ricardogcolombo <ricardogcolombo@gmail.com>
2017-05-25 14:17:46 -0300

    [ADD]metodo Agregar Rig en administradorRIGS

--- administradorRIGS.py
+++ administradorRIGS.py
@@ -1,31 +1,35 @@
 from politicasEleccionDeRig import PoliticaEleccionRigRandom
 
+
 class AdministradorRIGS:
-	def __init__(self):
-		self.rigsDisponibles = []
-		self.rigsUtilizados = []
-		#falta asignar politca
-		politicaDeAdministracion = PoliticaEleccionRigRandom()
 
-	def excavar(self,parcela):
-		rigAUtilizar = self.dameRig()
-		response = this.asignarRig(rigAUtilizar, parcela)
-		self.rigsUtilizados.append(rigAUtilizar)
+    def __init__(self):
+        self.rigsDisponibles = []
+        self.rigsUtilizados = []
+        # falta asignar politca
+        politicaDeAdministracion = PoliticaEleccionRigRandom()
 
-	def cantidadRigsDisponibles(self):
-		return len(self.rigsDisponibles)
+    def excavar(self, parcela):
+        rigAUtilizar = self.dameRig()
+        response = this.asignarRig(rigAUtilizar, parcela)
+        self.rigsUtilizados.append(rigAUtilizar)
 
+    def cantidadRigsDisponibles(self):
+        return len(self.rigsDisponibles)
 
-	def hayRigsDisponibles(self):
-		return len(self.rigsDisponibles) != 0
+    def hayRigsDisponibles(self):
+        return len(self.rigsDisponibles) != 0
 
-	def asignarRig(self,rig, parcela):
-		rig.asignarParcela(parcela)
+    def asignarRig(self, rig, parcela):
+        rig.asignarParcela(parcela)
 
-	def dameRig(self):
-		return politicaDeAdministracion.elegirRIG(self.rigsDisponibles)
+    def dameRig(self):
+        return politicaDeAdministracion.elegirRIG(self.rigsDisponibles)
 
-	def progresar(self):
-		for rig in self.rigsUtilizados:
-			rig.bajarDia() # verificar esto, por el tema de la responsabilidad
-			
+    def progresar(self):
+        for rig in self.rigsUtilizados:
+            rig.bajarDia()  # verificar esto, por el tema de la responsabilidad
+
+    #  Este metodo es para agregar un rig al simulador para ser utilizado
+    def agregarRig(rig):
+        self.rigsDisponibles.append(rig)
