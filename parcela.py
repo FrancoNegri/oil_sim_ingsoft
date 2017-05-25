0533f35ab71a822cfae4ebb57f767b74267b7502
ricardogcolombo <ricardogcolombo@gmail.com>
2017-05-25 14:15:24 -0300

    [ADD]separo parcela y yacimiento en dif archivos

---
+++ parcela.py
@@ -0,0 +1,9 @@
+class parcela:
+	def __init__(self,profundidadAlReservorio,precionInicial,tipoDeTerreno):
+		self.profundidadAlReservorio = profundidadAlReservorio
+		self.precionInicial = precionInicial
+		self.tipoDeTerreno = tipoDeTerreno
+
+	def profundidad():
+		return self.profundidad
+
--- yacimiento.py
+++ yacimiento.py
@@ -1,12 +1,3 @@
-class parcela:
-	def __init__(self,profundidadAlReservorio,precionInicial,tipoDeTerreno):
-		self.profundidadAlReservorio = profundidadAlReservorio
-		self.precionInicial = precionInicial
-		self.tipoDeTerreno = tipoDeTerreno
-
-	def profundidad():
-		return self.profundidad
-
 class yacimiento:
 	def __init__(self, conjuntoDeParcelas, cantidadDeAgua, cantidadDeGas, cantidadDePetroleo):
 		self.conjuntoDeParcelas = conjuntoDeParcelas
