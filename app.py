769c126b96a8952768801ee232d87be532ecc7ab
Patricio Tarantino <patriciotarantino@gmail.com>
2017-05-25 12:33:05 -0300

    Escribir en el log en el boton del click

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+from logger import Logger
+import time
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
+
+@app.route('/log')
+def log():
+	logger = Logger("log.txt")
+	logger.logear('Escrito a las %s' % time.strftime("%H:%M:%S"))
+	return 'Ok'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
