6c37575dd1eb421952f23c6922f5c53a80b01bca
ricardogcolombo <ricardogcolombo@gmail.com>
2017-05-27 20:12:48 -0300

    [ADD]parametro profundidad al reservorio de la parcela concreta

--- parcela.py
+++ parcela.py
@@ -2,7 +2,7 @@
 class terreno():
 	def dameResistencia():
 		return
-	
+
 class terrenoRocoso(terreno):
 	def dameResistencia():
 		return 0.6
@@ -10,7 +10,7 @@ class terrenoRocoso(terreno):
 class terrenoArcilloso(terreno):
 	def dameResistencia():
 		return 0.7
-		
+
 #Clases de Parcela
 class parcelaAbstracta():
 	def __init__(self)
@@ -19,10 +19,10 @@ class parcelaAbstracta():
 class parcelaNull(parcelaAbstracta):
 	def __init__(self)
 		return
-		
+
 class parcelaConcreta(parcelaAbstracta):
-	def __init__(self,yacimiento,presionInicial,tipoDeTerreno):
-		self.profundidadAlReservorio = yacimiento.profundidadAlReservorio
+	def __init__(self,yacimiento,presionInicial,tipoDeTerreno,profundidadAlReservorio):
+		self.profundidadAlReservorio = profundidadAlReservorio
 		self.yacimiento = yacimiento
 		self.presion = presionInicial
 		self.tipoDeTerreno = tipoDeTerreno
@@ -34,14 +34,14 @@ class parcelaConcreta(parcelaAbstracta):
 		volRestanteDelYacimiento = self.yacimiento.volumenRestante()
 		volInicialDelYacimiento = self.yacimiento.volumenInicial()
 		BETA = (0.1 * (volRestanteDelYacimiento/volInicialDelYacimiento))/sqrtCUBO(cantidadDePozos^2)
-		
+
 	def perforar(poderDeExcavacion)
 		self.profundidadAlReservorio -= poderDeExcavacion*tipoDeTerreno.resistencia()
 		if self.profundidadAlReservorio < 0:
 			self.pozo = pozoFinalizado(self)
-			
+
 	def listoParaExtraer()
 		return self.pozo.listoParaExtraer()
-		
+
 	def volumen():
 		return self.yacimiento.volumen()
