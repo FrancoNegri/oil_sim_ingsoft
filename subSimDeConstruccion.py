706fa399d5681a41b05d1c8134b424c5ab3b6948
Franco Negri <franconegri2004@gmail.com>
2017-05-27 15:35:58 -0300

    Agregado subsistema de construcción

--- subSimDeConstruccion.py
+++ subSimDeConstruccion.py
@@ -1,6 +1,56 @@
-class SubSimDeConstruccion:
-	def __init__(self, logger):
-		return
+class SubSimDeConstruccion():
+	def __init__(self,logger,politicaDeConstruccionDeTanques,politicaDeConstruccionDePlantas):
+		self.listaTanquesListos = []
+		self.listaTanquesConstruyendose = []
+		self.listaPlantasProcesadorasListas = []
+		self.listaPlantasProcesadorasConstruyendose = []
+		self.politicaDeConstruccionDeTanques = politicaDeConstruccionDeTanques
+		self.politicaDeConstruccionDePlantas = politicaDeConstruccionDePlantas
 
 	def simularConstruccion(self,dia):
-		return
+		#construyo tanques
+		nuevosTanquesAConstruir = self.empezarConstruccionDeEstructuras(dia,self.politicaDeConstruccionDeTanques)
+		self.listaTanquesConstruyendose = self.listaTanquesConstruyendose + nuevosTanquesAConstruir
+		self.pasarDia(self.listaTanquesConstruyendose)
+		self.listaTanquesListos = self.listaTanquesListos + estructurasFinalizadas(self.listaTanquesConstruyendose)
+		self.listaTanquesConstruyendose = estructurasNoFinalizadas(self.listaTanquesConstruyendose)
+		#construyo plantas
+		nuevasPlantasAConstruir = self.empezarConstruccionDeEstructuras(dia,self.politicaDeConstruccionDePlantas)
+		self.listaPlantasProcesadorasConstruyendose = self.listaPlantasProcesadorasConstruyendose + nuevasPlantasAConstruir
+		self.pasarDia(self.listaPlantasProcesadorasConstruyendose)
+		self.listaPlantasProcesadorasListas = self.listaTanquesListos + estructurasFinalizadas(self.listaPlantasProcesadorasConstruyendose)
+		self.listaPlantasProcesadorasConstruyendose = estructurasNoFinalizadas(self.listaPlantasProcesadorasConstruyendose)
+
+	def pasarDia(listaEstructuras):
+		for estructura in listaEstructuras:
+			estructura.pasarDia()		
+
+	def empezarConstruccionDeEstructuras(self,dia,politicaDeConstruccion):
+		estructurasAConstruir = []
+		cantidadDeEstructurasAConstruirHoy = politicaDeConstruccion.elegir(dia)
+		for i in range(0,cantidadDeEstructurasAConstruirHoy):
+			estructurasAConstruir.append(EstructuraEnConstrucción())
+		return estructurasAConstruir
+
+	def estructurasFinalizadas(self, listaEstructurasConstruyendose):
+		return list(filter(lambda estructura: estructura.construccionFinalizada(),listaEstructurasConstruyendose))
+
+	def estructurasNoFinalizadas(self, listaEstructurasConstruyendose):
+		return list(filter(lambda estructura: not estructura.construccionFinalizada(),listaEstructurasConstruyendose))
+		
+
+
+class EstructuraEnConstrucción():
+	def __init__(self,diasDeConstruccion,estructura):
+		self.diasDeConstruccion = diasDeConstruccion
+		self.estructura = estructura
+		self.diasConstruido = 0
+
+	def pasarDia():
+		self.diasConstruido += 1
+
+	def construccionFinalizada():
+		return self.diasConstruido == self.diasDeConstruccion
+
+	def estructura():
+		return estructura
--- subSimDeExtraccion.py
+++ subSimDeExtraccion.py
@@ -4,12 +4,12 @@ class SubSimDeExtraccion:
 		self.log=log
 		self.politicaDeExtraccion=politicaDeExtraccion
 		
-	def simularExtraccion(self,dia,plantasProcesdoras,tanques):
+	def simularExtraccion(self,plantasProcesdoras,tanques):
 		#	obtenemos las parcelas que tienen el pozo terminado
 		parcelasListasParaExtraer = list(filter(lambda rig: rig.parcela().listoParaExtraer()),self.parcelas))
 		#	Elegimos las parcelas a ser utilizadas en base a la politica
 		parcelasElegidas = politicaDeExtraccion.elegir(parcelasListasParaExtraer)
 		#	abrimos la valvula y procesamos la materia prima	
-		ListaEventosDeAperturaDeValvulas =  list(map(lambda parcela: parcela.pozo.abrirValvula(plantaProcesadoras),parcerlasElegidas))
+		ListaEventosDeAperturaDeValvulas =  list(map(lambda parcela: parcela.pozo.abrirValvula(plantasProcesdoras),parcerlasElegidas))
 		ListaDeEventosDeAlmacenamento = planteaProcesdora.liberar(tanques)
 		return ListaEventosDeAperturaDeValvulas + ListaDeEventosDeAlmacenamento
