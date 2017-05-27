eb9ef41747a999807d004796c52c577417478e5c
ricardogcolombo <ricardogcolombo@gmail.com>
2017-05-27 20:09:25 -0300

    [FIX]parcelaspara extraer vienen por parametro

--- subSimDeExtraccion.py
+++ subSimDeExtraccion.py
@@ -3,13 +3,13 @@ class SubSimDeExtraccion:
 		self.parcelas = parcelas
 		self.log=log
 		self.politicaDeExtraccion=politicaDeExtraccion
-		
-	def simularExtraccion(self,plantasProcesdoras,tanques):
+
+	def simularExtraccion(self,parcelasListasParaExtraer,plantasProcesdoras,tanques):
 		#	obtenemos las parcelas que tienen el pozo terminado
-		parcelasListasParaExtraer = list(filter(lambda parcela: parcela.listoParaExtraer()),self.parcelas))
+		#  parcelasListasParaExtraer = list(filter(lambda parcela: parcela.listoParaExtraer()),self.parcelas))
 		#	Elegimos las parcelas a ser utilizadas en base a la politica
 		parcelasElegidas = politicaDeExtraccion.elegir(parcelasListasParaExtraer)
-		#	abrimos la valvula y procesamos la materia prima	
+		#	abrimos la valvula y procesamos la materia prima
 		ListaEventosDeAperturaDeValvulas =  list(map(lambda parcela: parcela.pozo.abrirValvula(plantasProcesdoras),parcerlasElegidas))
 		ListaDeEventosDeAlmacenamento = planteaProcesdora.liberar(tanques)
 		return ListaEventosDeAperturaDeValvulas + ListaDeEventosDeAlmacenamento
