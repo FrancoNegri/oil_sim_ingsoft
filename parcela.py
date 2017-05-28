d810e959bedecd1df70b5394486c8af87dcd9c12
ricardogcolombo <ricardogcolombo@gmail.com>
2017-05-28 09:20:51 -0300

    [ADD] Ultimos cambios en kobra

--- parcela.py
+++ parcela.py
@@ -20,11 +20,13 @@ class parcelaNull(parcelaAbstracta):
 	def __init__(self)
 		return
 
+#parcela!
 class parcelaConcreta(parcelaAbstracta):
-	def __init__(self,yacimiento,presionInicial,tipoDeTerreno,profundidadAlReservorio):
-		self.profundidadAlReservorio = profundidadAlReservorio
+	def __init__(self,yacimiento,presionInicial,tipoDeTerreno):
+		self.profundidadAlReservorio = yacimiento.profundidadAlReservorio
 		self.yacimiento = yacimiento
 		self.presion = presionInicial
+		self.presionInicial = presionInicial
 		self.tipoDeTerreno = tipoDeTerreno
 		self.pozo = pozoNull()
 
@@ -34,20 +36,25 @@ class parcelaConcreta(parcelaAbstracta):
 		volRestanteDelYacimiento = self.yacimiento.volumenRestante()
 		volInicialDelYacimiento = self.yacimiento.volumenInicial()
 		BETA = (0.1 * (volRestanteDelYacimiento/volInicialDelYacimiento))/sqrtCUBO(cantidadDePozos^2)
-	
+
 	def perforar(poderDeExcavacion)
-		perforarUnaDistanciaDe(self, poderDeExcavacion*tipoDeTerreno.resistencia())
-		finalizarPozoSiProfundidadEsNecesaria(self)
-	
+	    perforarUnaDistanciaDe(self, poderDeExcavacion*tipoDeTerreno.resistencia())
+	    finalizarPozoSiProfundidadEsNecesaria(self)
+
 	def perforarUnaDistanciaDe(self,unaDistancia)
-		self.profundidadAlReservorio -= unaDistancia
-	    
+	    self.profundidadAlReservorio -= unaDistancia
+
 	def finalizarPozoSiProfundidadEsNecesaria(self)
 		if self.profundidadAlReservorio < 0:
-			self.pozo = pozoFinalizado(self)
+	        self.pozo = pozoFinalizado(self)
 
 	def listoParaExtraer()
 		return self.pozo.listoParaExtraer()
 
 	def volumen():
 		return self.yacimiento.volumen()
+
+	def reinyectar(volumenAgua,volumenGas):
+	    #falta hacer el chequeo de reinyeccion sarasa, posiblemente en la politica? ya no tengo idea
+	    presionDespuesDeReinyeccion = self.presionInicial *( self.yacimiento.volumenInicial() - self.yacimiento.volumenExtraido() + self.yacimiento.volumenReinyectado())/ self.yacimiento.volumenInicial()
+	    self.yacimiento.reinyectar(volumenAgua,volumenGas)
--- yacimiento.py
+++ yacimiento.py
@@ -1,4 +1,3 @@
-#yacimiento
 class yacimiento:
 	def __init__(self,parcelas,profunidadAlReservorio,volumenInicial,proporcionGas,proporcionAgua,proporcionPetroleo):
 		self.parcelas=parcelas
@@ -8,9 +7,17 @@ class yacimiento:
 		self.proporcionGas = proporcionGas
 		self.proporcionAgua=proporcionAgua
 		self.proporcionPetroleo=proporcionPetroleo
+		self.volumenReinyectado = 0
 	def extreaerProducto(volumen):
 		self.volumenRestante-=volumen
 	def volumenRestante():
 		return self.volumenRestante
 	def volumenInicial():
 		return self.volumenInicial
+	def volumenExtraido():
+		return self.volumenInicial - self.volumenRestante
+	def reinyectar(volumenAgua,volumenGas):
+	    self.volumenReinyectado += volumenAgua + volumenGas
+	    self.proporcionAgua = (self.proporcionAgua * ((self.volumenInicial() - self.volumenExtraido) + 100* self.volumenReinyectado()))/(self.volumenInicial() - self.volumenExtraido() + self.volumenReinyectado)
+	    self.proporcionGas = (self.proporcionGas * (self.volumenInicial() - self.volumenExtraido) )/(self.volumenInicial() - self.volumenExtraido() + self.volumenReinyectado
+	    self.proporcionPetroleo = (self.proporcionPetroleo * ((self.volumenInicial() - self.volumenExtraido) )/(self.volumenInicial() - self.volumenExtraido() + self.volumenReinyectado)
