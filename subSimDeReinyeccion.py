9f6b09be83953d649f8668273f3acef019d98184
shinso1990 <alancastro90@gmail.com>
2017-05-28 22:03:36 -0300

    reinyección
    faltaba devolver eventos de reinyección

--- subSimDeReinyeccion.py
+++ subSimDeReinyeccion.py
@@ -8,5 +8,5 @@ class SubSimDeReinyeccion:
 		# elegir cuando producto se va a reinyectar en cada pozo, salksakdkask
 		parcelasAReinyectar = self.politicaDeQueParcelasReinyectar.elegir(parcelasConPozo,dia)
 		eventos = list(map(lambda parcela: parcela.reiyectar() ,parcelasAReinyectar))
-		return
+		return eventos
 		
