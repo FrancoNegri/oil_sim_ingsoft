71a98ccd4c4869d332cce7750f339890f766098d
shinso1990 <alancastro90@gmail.com>
2017-05-29 20:38:50 -0300

    agregado functools

--- calculadorDeGanancia.py
+++ calculadorDeGanancia.py
@@ -1,6 +1,7 @@
+import functools
 class CalculadorDeGanancia():
 	def calcular(self,eventos):
 		listaGanancias = list(map(lambda evento: evento.getGanancia(),eventos))
 		if listaGanancias:
-			gananciaTotal = reduce(lambda x,y: x+y,listaGanancias)
+			gananciaTotal = functools.reduce(lambda x,y: x+y,listaGanancias)
 		return gananciaTotal
