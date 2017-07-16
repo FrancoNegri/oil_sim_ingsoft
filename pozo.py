fc89d8d1d48baf8f3b2abb9dad75a87a3dca4cd3
ricardogcolombo <ricardogcolombo@gmail.com>
2017-07-16 20:31:19 -0300

    [ADD]getVolumenPotencial

--- pozo.py
+++ pozo.py
@@ -11,23 +11,25 @@ class PozoNull(PozoAbstracto):
 		return
 	def listoParaExtraer(self):
 		return False
-		
+
 class PozoFinalizado(PozoAbstracto):
 	def __init__(self, parcela):
 		self.parcela = parcela
 		self.alfa1 = 0.4
 		self.alfa2 = 0.3
 		return
-	
+
 	def listoParaExtraer(self):
 		return True
+    def getVolumenPotencial(self,presion,cantidadDePozosHabilitados):
+        return self.alfa1*(presion/cantidadDePozosHabilitados) + self.alfa2*(presion/cantidadDePozosHabilitados)**2
 
 	def abrirValvula(self,cantidadDePozosHabilitados,plantasProcesadoras,tanques):
 		presion = self.parcela.getPresion()
 		#falta decidir si alfa1 y alfa2 se pasan por parametro o de donde salen
-		volumenPotencial = self.alfa1*(presion/cantidadDePozosHabilitados) + self.alfa2*(presion/cantidadDePozosHabilitados)**2
+        volumenPotencial = self.getVolumenPotencial(presion,cantidadDePozosHabilitados)
 		volumenTotalDelYacimiento = self.parcela.getVolumenRestante()
-		#si el volumen 
+		#si el volumen
 		if volumenPotencial > volumenTotalDelYacimiento:
 			volumenPotencial = volumenTotalDelYacimiento
 		volumenTotalProcesado = 0
